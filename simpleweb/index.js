ae434ef59c863c804f317682dbeee5b3a4038165
rajibreturns <gaurab.dawn@gmail.com>
2020-11-08 15:55:28 +0530

    3rd commit

---
+++ simpleweb/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+								res.send('Bye There'); 
+						  } );
+								
+								
+app.listen(8080, () => {
+							console.log('Listening on port 8080');
+});
+								
